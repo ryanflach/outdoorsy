b1258e8e89309fe2fa9cc68129104fbb3f02df4f
Ryan Flach <ryanflach@users.noreply.github.com>
2022-08-30 15:41:10 -0700

    Remove UUID to preserve active storage behavior

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_220708) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_221229) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
